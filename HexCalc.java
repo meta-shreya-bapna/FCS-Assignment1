43cc028cc82fd8188be2feced7eaa8d17de4a283
Shreya Bapna <shreya.bapna@metacube.com>
2021-03-04 09:25:03 +0530

    Changed to generic implementation

--- HexCalc.java
+++ HexCalc.java
@@ -4,35 +4,46 @@ import java.util.Scanner;
 
 public class HexCalc {
 	
-	public static void add(int value1,int value2)
+	public static int[] convert(String number1,String number2,int base)
 	{
-		int sum = value1 + value2;
-		System.out.println(Integer.toHexString(sum).toUpperCase());
+		int[] number=new int[2];
+		number[0] =Integer.parseInt(number1,base);
+		number[1] =Integer.parseInt(number2,base);
+		return(number);
 	}
 	
-	public static void subtract(int value1,int value2)
-	{
-		int subtraction = Math.abs(value1 - value2);
-		System.out.println(Integer.toHexString(subtraction).toUpperCase());
+	public static String add(String num1,String num2,int base)
+	{	int []value= convert(num1,num2,base);
+		
+		int sum = value[0] + value[1];
+		return(Integer.toString(sum, base).toUpperCase());
 	}
 	
-	public static void multiply(int value1,int value2)
+	public static String subtract(String num1,String hex2,int base)
 	{
-		long product = value1 * value2;
-		System.out.println(Long.toHexString(product).toUpperCase());
+		int []value= convert(num1,hex2,base);
+		int subtraction = Math.abs(value[0] - value[1]);
+		return(Integer.toString(subtraction, base).toUpperCase());
 	}
 	
-	public static void divide(int value1,int value2)
+	public static String multiply(String num1,String hex2,int base)
 	{
-		int quotient = value1/ value2;
-		int remainder = value1 % value2; 
-		System.out.println("Quotient= "+Integer.toHexString(quotient).toUpperCase()+" and "+" remainder= "+ Integer.toHexString(remainder).toUpperCase());
+		int []value= convert(num1,hex2,base);
+		long product = value[0] * value[1];
+		return(Long.toString(product, base).toUpperCase());
+	}
+	
+	public static String divide(String num1,String hex2,int base)
+	{
+		int []value= convert(num1,hex2,base);
+		int quotient = value[0]/ value[1]; 
+		return(Integer.toString(quotient, base).toUpperCase());
 	}
 	
 	public static void compare()
 	{
 		Scanner sc= new Scanner(System.in);
-		System.out.println("Enter 2 hexadecimals to compare");
+		System.out.println("Enter 2 numbers to compare");
 		String hexToCompareValue1 = sc.next().toUpperCase();
 		String hexToCompareValue2 =sc.next().toUpperCase();
 		System.out.println("Comparing "+ hexToCompareValue1+" and "+ hexToCompareValue2+":");
@@ -49,55 +60,57 @@ public class HexCalc {
 
 	public static void main(String[] args) {
 		boolean flag=true;
-		int choice,hex1,hex2;
+		int choice;
+		String result="";
 		Scanner sc = new Scanner(System.in);
 		
 		while(flag==true){
-			System.out.println("1.Add, sub, multiply or divide 2 hexadecimal numbers");
-			System.out.println("2.Compare 2 hexadecimal numbers");
-			System.out.println("3.Decimal number to hexadecimal conversion");
-			System.out.println("4.Hexadecimal to decimal conversion");
+			System.out.println("1.Add, sub, multiply or divide 2 numbers");
+			System.out.println("2.Compare 2 numbers");
+			System.out.println("3.For conversion");
 			System.out.println("5.Exit");
 		
 			choice= sc.nextInt();
 			switch(choice)
 			{
 			case 1:{
-				System.out.println("Enter the first hexa decimal number");
+				System.out.println("Enter the base to use");
+				int base=sc.nextInt();
+				System.out.println("Enter the first number");
 				String number1= sc.next();
-				System.out.println("Enter the second hexa decimal number");
+				System.out.println("Enter the second number");
 				String number2= sc.next();
+				
 				System.out.println("Press 1: add\t 2: sub\t 3: mult\t 4: divide ");
 				int operationChoice= sc.nextInt();
-				hex1 =Integer.parseInt(number1,16);
-				hex2 =Integer.parseInt(number2,16);
+
 				
 				switch(operationChoice){
 				case 1:
 					
-					add(hex1,hex2);
+					result=add(number1,number2,base);
 					break;
 				
 				
 				case 2:
 					
-					subtract(hex1, hex2);
+					result= subtract(number1,number2,base);
 					break;
 					
 				case 3:
 					
-					multiply(hex1,hex2);
+					result=multiply(number1,number2,base);
 					break;
 				
 				case 4:
 					
-					divide(hex1,hex2);
+					result=divide(number1,number2,base);
 					break;
 					
 				default:
 					System.out.println("Invalid input number");
 				}
-			
+			System.out.println("Result is : "+ result);
 			}
 			break;
 			
@@ -106,17 +119,26 @@ public class HexCalc {
 				break;
 				
 			case 3:
-				int decimalToHex= sc.nextInt();
-				System.out.println(Integer.toHexString(decimalToHex).toUpperCase());
+				
+				System.out.println("Enter the number with base to convert");
+				String num= sc.next();
+				int base1 =sc.nextInt();
+				int number =Integer.parseInt(num,base1);
+				System.out.println("\nEnter the number to convert the base into");
+				int base2=sc.nextInt();
+				
+				System.out.println("Result is: "+Integer.toString(number, base2).toUpperCase()+"\n");
 				break;
 				
 			case 4:
+				
 				String hexaToDecimal=sc.next();
 				int decimal= Integer.parseInt(hexaToDecimal,16);
 				System.out.println(decimal);
 				break;
 				
 			case 5:
+				
 				flag= false;
 				break;
 				
